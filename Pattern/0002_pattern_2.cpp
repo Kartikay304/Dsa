1475e052164b6e91f6ce5caa5ab0a01b4180c0fa
kartikay Kala <40604582+Kartikay304@users.noreply.github.com>
2026-02-03 23:43:56 +0530

    pattern_2.cpp

---
+++ Pattern/0002_pattern_2.cpp
@@ -0,0 +1,20 @@
+// 0002_pattern_2.cpp
+/*   N=5=>  *
+            **
+            ***
+            ****
+            *****
+*/
+// URL: https://takeuforward.org/plus/dsa/problems/pattern-2?category=beginner-problem&subcategory=patterns
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<=i;j++) cout<<"* ";
+        cout<<endl;
+    }
+}
