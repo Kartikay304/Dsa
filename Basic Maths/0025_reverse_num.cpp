38bc955b53be848f6d9091d4d011bc8a6457da5d
kartikay Kala <40604582+Kartikay304@users.noreply.github.com>
2026-02-04 01:45:11 +0530

    reverse_num.cpp

---
+++ Basic Maths/0025_reverse_num.cpp
@@ -0,0 +1,24 @@
+// 0025_reverse_num.cpp
+
+// Problem: Reverse a Number
+// URL: https://takeuforward.org/plus/dsa/problems/count-number-of-odd-digits-in-a-number?category=beginner-problem&subcategory=basic-maths
+// Goal: Return the integer formed by placing digits in reverse order
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int reverseNum(int n) {
+    int reverse_num=0;
+    while(n>0) {
+        reverse_num*=10;
+        reverse_num += n%10;
+        n/=10;
+    }
+    return reverse_num;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<reverseNum(n);
+}
