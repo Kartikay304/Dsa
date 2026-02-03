01d0e2e014c8aa64d72f4111fa09b2f5504c2ada
kartikay Kala <40604582+Kartikay304@users.noreply.github.com>
2026-02-03 23:46:32 +0530

    pattern_3.cpp

---
+++ Pattern/3.cpp
@@ -0,0 +1,20 @@
+// 0003_pattern_3.cpp
+/*   N=5=>  1
+            12
+            123
+            1234
+            12345
+*/
+//URL: https://takeuforward.org/plus/dsa/problems/pattern-3?category=beginner-problem&subcategory=patterns
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++) {
+        for(int j=0; j<=i;j++) cout<<j+1<<" ";
+        cout<<endl;
+    }
+}
