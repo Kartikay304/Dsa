5c35fe53754d93bb1b1428208c3c45551674c8f0
kartikay Kala <40604582+Kartikay304@users.noreply.github.com>
2026-02-04 00:05:00 +0530

    pattern_8.cpp

---
+++ Pattern/0008_pattern_8.cpp
@@ -0,0 +1,22 @@
+//0008_pattern_8.cpp
+
+/* N=5=>  *********
+           *******
+            *****
+             ***
+              *
+*/
+//URL:https://takeuforward.org/plus/dsa/problems/pattern-8?category=beginner-problem&subcategory=patterns
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<i;j++) cout<<" ";
+        for(int j=0;j<(2*n-1)-(2*i);j++) cout<<"*";
+        cout<<endl;
+    }
+}
