271d56a17798266afd83313132c0bf9303d272ba
kartikay Kala <40604582+Kartikay304@users.noreply.github.com>
2026-02-04 11:50:03 +0530

    prime_no.cpp

--- Basic Maths/0031_prime_no.cpp
+++ Basic Maths/0031_prime_no.cpp
@@ -10,7 +10,7 @@ using namespace std;
 bool isPrime(int n) {
     if(n==1 || n==0) return 0;
     for(int i=2;i*i<=n;i++) {
-        if(n%2==0) return 0;
+        if(n%i==0) return 0;
     }
     return 1;
 }
