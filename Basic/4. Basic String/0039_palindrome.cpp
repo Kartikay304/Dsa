da7eacdda472fd0c33fd0d9832ae08f1484525ab
kartikay Kala <40604582+Kartikay304@users.noreply.github.com>
2026-02-07 21:36:55 +0530

    palindrome.cpp

---
+++ Basic/4. Basic String/0039_palindrome.cpp
@@ -0,0 +1,30 @@
+// 0038_palindrome.cpp
+
+// Problem: Palindrome Check
+// URL: https://takeuforward.org/plus/dsa/problems/palindrome-check?category=beginner-problem&subcategory=basic-strings
+// Goal: You are given a string s. Return true if the string is palindrome, otherwise false.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool palindromeCheck(vector<char>& vec, int i, int j) {
+    while(i<=j) {
+        if(vec[i]!=vec[j]) return false;
+        i++; j--;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    char num;
+    cin>>n;
+    vector <char> vec;
+    for(int i=0;i<n;i++) {
+        cin>>num;
+        vec.push_back(num);
+    }
+    if(palindromeCheck(vec,0,n-1)) cout<<"True";
+    else cout<<"False";
+    return 0;
+}
