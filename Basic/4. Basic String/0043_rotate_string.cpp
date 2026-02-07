b745d0281a16a030d4dd6d46ee18b93e9afac8f0
kartikay Kala <40604582+Kartikay304@users.noreply.github.com>
2026-02-07 23:47:57 +0530

    rotate_string.cpp

---
+++ Basic/4. Basic String/0043_rotate_string.cpp
@@ -0,0 +1,23 @@
+// 0043_rotate_string.cpp
+
+// Problem: Rotate String
+// URL: https://takeuforward.org/plus/dsa/problems/rotate-string?category=beginner-problem&subcategory=basic-strings
+// Goal: Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkRotate(string& s, string& goal) {
+    if(s.size()!=goal.size()) return false;
+    string doubleS=s+s;
+    return doubleS.find(goal) != string::npos;
+
+}
+
+int main() {
+    string s, goal;
+    cin>>s>>goal;
+    if(checkRotate(s,goal)) cout<<"True";
+    else cout<<"False";
+    return 0;
+}
