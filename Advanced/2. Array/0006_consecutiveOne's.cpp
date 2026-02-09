aa52f098021d6b4010e5dc53a38b4da43913a1f9
kartikay Kala <40604582+Kartikay304@users.noreply.github.com>
2026-02-09 19:01:29 +0530

    consecutive one.cpp

---
+++ Advanced/2. Array/0006_consecutiveOne's.cpp
@@ -0,0 +1,31 @@
+// 0006_consecutive_one.cpp
+
+// Problem: Maximum Consecutive Ones
+// URL: https://takeuforward.org/plus/dsa/problems/maximum-consecutive-ones?category=arrays&subcategory=fundamentals
+// Goal: Given a binary array nums, return the maximum number of consecutive 1s in the array.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int consicutiveOne(vector<int>& vec, int n) {
+    int count=0,maxCount=0;
+    for(int i=0;i<n;i++) {
+        if(vec[i]==1) count++;
+        else {
+            maxCount=max(maxCount,count);
+            count=0;
+        }
+    }
+    return maxCount;
+}
+
+int main() {
+    int n,num;
+    vector<int> vec;
+    cin>>n;
+    for(int i=0;i<n;i++) {
+        cin>>num;
+        vec.push_back(num);
+    }
+    cout<<"Maximum no of consicutive 1 is:: "<<consicutiveOne(vec,n);
+}
