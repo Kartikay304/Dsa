0e5b0b06f61a6cc7d773494ce52d260179248660
kartikay Kala <40604582+Kartikay304@users.noreply.github.com>
2026-02-09 16:39:03 +0530

    modified insertion sort

---
+++ Advanced/1. Sorting/0003_insertionSort_modified.cpp
@@ -0,0 +1,33 @@
+// 0002_insertion_sort.cpp
+
+// Problem: Insertion Sort
+// URL: https://takeuforward.org/plus/dsa/problems/insertion-sorting?category=sorting&subcategory=algorithms
+// Goal: Sort the array in non-decreasing order using the insertion sort algorithm and return the sorted array.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(vector <int>& vec) {
+    for(int i=1;i<vec.size();i++) {
+        int key = vec[i];
+        int j=i-1;
+        while(j>=0 && vec[j]>key) {
+            if(vec[j]>key) vec[j+1]=vec[j];
+            j--;
+        }
+        vec[j+1]=key;
+    }
+}
+
+int main() {
+    int n,num;
+    vector <int>vec;
+    cin>>n;
+    for(int i=0;i<n;i++) {
+        cin>>num;
+        vec.push_back(num);
+    }
+    insertionSort(vec);
+    for(auto it : vec) cout<<it<<" ";
+    return 0;
+}
