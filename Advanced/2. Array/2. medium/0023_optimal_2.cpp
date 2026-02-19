4af98298d45734f68c50235ed82ef490b3e10b18
Kartikay Kala <kartikay304@gmail.com>
2026-02-19 15:07:22 +0530

    four sum optimal

--- Advanced/2. Array/2. medium/0023_optimal_2.cpp
+++ Advanced/2. Array/2. medium/0023_optimal_2.cpp
@@ -8,23 +8,28 @@
 using namespace std;
 
 vector<vector<int>> fourSum(vector<int> nums,int target) {
+    vector<vector<int>> ans;
     int n=nums.size();
-    set<vector<int>> quadrupleSet;
-    for(int i=0;i<n-3;i++) {
-        for(int j=i+1;j<n-2;j++) {
-            for(int k=j+1;k<n-1;k++) {
-                for(int l=k+1;l<n;l++) {
-                    if(nums[i]+nums[j]+nums[k]+nums[l]==target) {
-                        vector<int> temp={nums[i],nums[j],nums[k],nums[l]};
-                        sort(temp.begin(),temp.end());
-                        quadrupleSet.insert(temp);
-                    }
-                }
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<n;i++){
+    while(i>0 && nums[i]==nums[i-1]) i++;
+        for(int j=i+1;j<n;j++) {
+            while(j>i+1 && nums[j]==nums[j-1]) j++;
+            int left=j+1,right=n-1;
+            while(left<right) {
+                int sum=nums[i]+nums[j]+nums[left]+nums[right];
+                if(sum==target) {
+                    vector<int> temp = {nums[i],nums[j],nums[left],nums[right]};
+                    ans.push_back(temp);
+                    left++; right--;
+                    while(left<right && nums[left]==nums[left-1]) left++;
+                    while(right>left && nums[right]==nums[right+1]) right--;
+                } else if(sum<target) left++;
+                else right--;
             }
         }
     }
-    vector<vector<int>> ans(quadrupleSet.begin(),quadrupleSet.end());
-    return ans;
+   return ans;
 }
 
 int main() {
